e85f76bd3e6dd9455fe5ba7ee714a8e73d0e085f
Luke Woods <lukedwoods@gmail.com>
2016-06-12 16:29:30 +0100

    capitalize first letter, words in sentence

---
+++ jaden_case_strings.py
@@ -0,0 +1,27 @@
+"""
+Description:
+
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010)
+and After Earth (2013). Jaden is also known for some of his philosophy that he delivers
+via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+
+Your task is to convert strings to how they would be written by Jaden Smith.
+
+The strings are actual quotes from Jaden Smith, but they are not capitalized in the same
+ way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+Note that the Java version expects a return value of null for an empty string or null.
+"""
+
+# first attempt
+def toJadenCase(string):
+    return ' '.join([word[0].upper()+word[1:] for word in string.split(' ')])
+
+
+# refined
+def toJadenCase2(string):
+    return ' '.join([word.capitalize() for word in string.split(' ')])
